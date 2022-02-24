e962a1acd88ec813ad245b76f7a6a2c53ca60ef3
Jeddchoi <jed.gy.choi@gmail.com>
2022-02-24 21:14:16 +0900

    add bottom navigation

--- app/src/main/java/jed/choi/seatreservation/MainActivity.kt
+++ app/src/main/java/jed/choi/seatreservation/MainActivity.kt
@@ -2,10 +2,13 @@ package jed.choi.seatreservation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jed.choi.seatreservation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
