d43167eaed3277aaaeff0b5a721a0792de4aa8cc
Jeddchoi <jed.gy.choi@gmail.com>
2022-02-24 21:50:06 +0900

    add splash screen compat api

--- app/src/main/java/jed/choi/seatreservation/MainActivity.kt
+++ app/src/main/java/jed/choi/seatreservation/MainActivity.kt
@@ -2,11 +2,15 @@ package jed.choi.seatreservation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import jed.choi.seatreservation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        // This needed to be here before super.onCreate()
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
