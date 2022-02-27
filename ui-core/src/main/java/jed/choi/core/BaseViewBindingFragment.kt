2058c763537d143c71dddb708e312b61821c7fc1
Jeddchoi <jed.gy.choi@gmail.com>
2022-02-27 16:07:04 +0900

    rename module names

---
+++ ui-core/src/main/java/jed/choi/core/BaseViewBindingFragment.kt
@@ -0,0 +1,35 @@
+package jed.choi.core
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseViewBindingFragment<VB : ViewBinding, VM : ViewModel> : Fragment(), ScrollableToTop {
+    protected val TAG = this::class.simpleName
+
+    private var _viewBinding: VB? = null
+    protected val viewBinding: VB
+        get() = _viewBinding!!
+
+    protected abstract val viewModel: VM
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _viewBinding = getBinding(inflater, container)
+        return viewBinding.root
+    }
+
+    override fun onDestroyView() {
+        _viewBinding = null
+        super.onDestroyView()
+    }
+
+    abstract fun getBinding(inflater: LayoutInflater, container: ViewGroup?): VB
+}
---
+++ ui-history/src/main/java/jed/choi/history/HistoryFragment.kt
@@ -0,0 +1,31 @@
+package jed.choi.history
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
+class HistoryFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HistoryFragment()
+    }
+
+    private lateinit var viewModel: HistoryViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.history_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HistoryViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
