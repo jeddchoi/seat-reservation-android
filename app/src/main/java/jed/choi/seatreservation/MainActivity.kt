ea2f0eec99ea50ba0da7a07aaa87d45bb56729e2
Jeddchoi <jed.gy.choi@gmail.com>
2022-02-28 10:03:11 +0900

    added BaseDataBindingActivity.kt & SlidingUpPanel

--- app/src/main/java/jed/choi/seatreservation/MainActivity.kt
+++ app/src/main/java/jed/choi/seatreservation/MainActivity.kt
@@ -2,51 +2,103 @@ package jed.choi.seatreservation
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.databinding.BindingAdapter
+import androidx.databinding.InverseBindingAdapter
+import androidx.databinding.InverseBindingListener
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.sothree.slidinguppanel.SlidingUpPanelLayout
 import dagger.hilt.android.AndroidEntryPoint
-import jed.choi.core.BaseViewBindingActivity
+import jed.choi.core.BaseDataBindingActivity
 import jed.choi.core.ScrollableToTop
 import jed.choi.seatreservation.databinding.ActivityMainBinding
 
 
 @AndroidEntryPoint
-class MainActivity : BaseViewBindingActivity<ActivityMainBinding, MainViewModel>() {
+class MainActivity : BaseDataBindingActivity<ActivityMainBinding, MainViewModel>() {
     override val viewModel: MainViewModel by viewModels()
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // This needed to be here before super.onCreate()
         val splashScreen = installSplashScreen()
 
         super.onCreate(savedInstanceState)
-        setContentView(viewBinding.root)
+        setContentView(dataBinding.root)
+        dataBinding.viewModel = viewModel
+        dataBinding.lifecycleOwner = this
 
         setupUi()
     }
 
     private fun setupUi() {
-        navHostFragment = viewBinding.fragmentContainer.getFragment()
+        navHostFragment = dataBinding.fragmentContainer.getFragment()
         navController = navHostFragment.navController
 
         setupBottomNavigation()
+        setupMySeatPanel()
+    }
+
+    private fun setupMySeatPanel() {
+
     }
 
 
-
     private fun setupBottomNavigation() {
-        viewBinding.bottomNavigation.setupWithNavController(navController)
+        dataBinding.bottomNavigation.setupWithNavController(navController)
 
-        viewBinding.bottomNavigation.setOnItemReselectedListener {
+        dataBinding.bottomNavigation.setOnItemReselectedListener {
             (navHostFragment.childFragmentManager.fragments.firstOrNull() as? ScrollableToTop)?.scrollToTop()
         }
     }
 
-    override fun getBinding(inflater: LayoutInflater): ActivityMainBinding = ActivityMainBinding.inflate(inflater)
+    override fun getBinding(inflater: LayoutInflater): ActivityMainBinding =
+        ActivityMainBinding.inflate(inflater)
+
+}
+
+
+object BindingAdapter {
+    @JvmStatic
+    @BindingAdapter("slideState")
+    fun setSlideState(view: SlidingUpPanelLayout, newValue: SlidingUpPanelLayout.PanelState?) {
+        // Important to break potential infinite loops.
+        val old = view.panelState
+        if (old != newValue && newValue != null) {
+            view.panelState = newValue
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter(value = ["slideStateAttrChanged"], requireAll = false)
+    fun bindSlideState(view: SlidingUpPanelLayout, listener: InverseBindingListener) {
+        val newListener =
+            object : SlidingUpPanelLayout.PanelSlideListener {
+                override fun onPanelSlide(panel: View?, slideOffset: Float) {
+                }
+
+                override fun onPanelStateChanged(
+                    panel: View?,
+                    previousState: SlidingUpPanelLayout.PanelState?,
+                    newState: SlidingUpPanelLayout.PanelState?
+                ) {
+                    if (previousState != newState && newState != SlidingUpPanelLayout.PanelState.DRAGGING) {
+                        listener.onChange()
+                    }
+                }
+            }
+        view.addPanelSlideListener(newListener)
+    }
+
+    @JvmStatic
+    @InverseBindingAdapter(attribute = "slideState", event = "slideStateAttrChanged")
+    fun getSlideState(view: SlidingUpPanelLayout): SlidingUpPanelLayout.PanelState {
+        return view.panelState
+    }
 
 }
--- app/src/main/java/jed/choi/seatreservation/MainViewModel.kt
+++ app/src/main/java/jed/choi/seatreservation/MainViewModel.kt
@@ -1,7 +1,22 @@
 package jed.choi.seatreservation
 
 import androidx.lifecycle.ViewModel
+import com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelState
+import jed.choi.domain.MySeatUiState
+import jed.choi.domain.UserSession
+import jed.choi.domain.UserState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class MainViewModel : ViewModel() {
+    val _mySeatUiState = MutableStateFlow(MySeatUiState("Jed Choi", UserState.LOGGED_IN, mySession = UserSession()))
+    val mySeatUiState : StateFlow<MySeatUiState>
+        get() = _mySeatUiState
+
+    val slidePanelState = MutableStateFlow(PanelState.COLLAPSED)
+
+//    init {
+//        slidePanelState.update { PanelState.HIDDEN }
+//    }
 
 }
