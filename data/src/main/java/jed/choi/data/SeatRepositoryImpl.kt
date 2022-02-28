73ab2944b63d9ed5388cbbc97322fa182c13d95d
Jeddchoi <jed.gy.choi@gmail.com>
2022-02-28 16:01:40 +0900

    before adding room

---
+++ data/src/main/java/jed/choi/data/SeatRepositoryImpl.kt
@@ -0,0 +1,13 @@
+package jed.choi.data
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SeatRepositoryImpl @Inject constructor() {
+
+
+    suspend fun modifyData(example: String) {
+
+    }
+}
--- ui_core/src/main/java/jed/choi/ui_core/UiState.kt
+++ ui_core/src/main/java/jed/choi/ui_core/UiState.kt
@@ -1,7 +1,7 @@
 package jed.choi.ui_core
 
 interface UiState {
-    val userMessages: List<UserMessage>
+
 }
 
 data class UserMessage(val id: Long, val message: String)
