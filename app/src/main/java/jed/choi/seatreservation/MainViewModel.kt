2a8693147657d1a18cda15d994399a1149f18183
Jeddchoi <jed.gy.choi@gmail.com>
2022-02-28 11:27:13 +0900

    add SeatRepository.kt

--- app/src/main/java/jed/choi/seatreservation/MainViewModel.kt
+++ app/src/main/java/jed/choi/seatreservation/MainViewModel.kt
@@ -8,13 +8,14 @@ import jed.choi.domain.UserState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class MainViewModel : ViewModel() {
+class MainViewModel(
+
+) : ViewModel() {
     val _mySeatUiState = MutableStateFlow(MySeatUiState("Jed Choi", UserState.LOGGED_IN, mySession = UserSession()))
     val mySeatUiState : StateFlow<MySeatUiState>
         get() = _mySeatUiState
 
     val slidePanelState = MutableStateFlow(PanelState.COLLAPSED)
-    val slidePanelOffset = MutableStateFlow(0f)
 
 //    init {
 //        slidePanelState.update { PanelState.HIDDEN }
---
+++ domain/src/main/java/jed/choi/domain/seat/SeatRepository.kt
@@ -0,0 +1,9 @@
+package jed.choi.domain.seat
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SeatRepository @Inject constructor() {
+
+}
