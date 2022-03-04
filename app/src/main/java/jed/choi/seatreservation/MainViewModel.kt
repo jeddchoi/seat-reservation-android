5499532570e7cdf225c3229e3cde1fdcdcf91d5b
Jeddchoi <jed.gy.choi@gmail.com>
2022-03-04 16:02:37 +0900

    fix screenorientation userPortrait

--- app/src/main/java/jed/choi/seatreservation/MainViewModel.kt
+++ app/src/main/java/jed/choi/seatreservation/MainViewModel.kt
@@ -1,6 +1,5 @@
 package jed.choi.seatreservation
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelState
@@ -8,7 +7,9 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import jed.choi.domain.usecase.AddUserMessage
 import jed.choi.domain.usecase.GetUserMessage
 import jed.choi.domain.usecase.RemoveUserMessage
+import jed.choi.seatreservation.model.MySeatUiState
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -18,10 +19,12 @@ class MainViewModel @Inject constructor(
     private val getUserMessage: GetUserMessage,
     private val removeUserMessage: RemoveUserMessage,
     private val addUserMessage: AddUserMessage,
+
 ) : ViewModel() {
-//    private val _mySeatUiState = MutableStateFlow(MySeatUiState("Jed Choi", UserState.LOGGED_IN, mySession = UserSession()))
-//    val mySeatUiState : StateFlow<MySeatUiState>
-//        get() = _mySeatUiState
+
+    private val _mySeatUiState = MutableStateFlow(MySeatUiState())
+    val mySeatUiState : StateFlow<MySeatUiState>
+        get() = _mySeatUiState
 
     val userMessage = getUserMessage.invoke().distinctUntilChanged()
     val slidePanelState = MutableStateFlow(PanelState.COLLAPSED)
@@ -32,14 +35,13 @@ class MainViewModel @Inject constructor(
 //        slidePanelState.update { PanelState.HIDDEN }
     }
 
-    fun addRandomUserMessage() {
+    fun testUserMessage() {
         viewModelScope.launch {
             addUserMessage.invoke("No Internet connection")
         }
     }
 
     fun userMessageShown(messageId: Long) {
-        Log.i("SnackBar", "userMessageShown: $messageId")
         viewModelScope.launch {
             removeUserMessage.invoke(messageId)
         }
--- app/src/main/java/jed/choi/seatreservation/model/MySeatUiState.kt
+++ app/src/main/java/jed/choi/seatreservation/model/MySeatUiState.kt
@@ -3,13 +3,11 @@ package jed.choi.seatreservation.model
 import jed.choi.domain.Seat
 import jed.choi.domain.UserSession
 import jed.choi.domain.UserState
-import jed.choi.domain.entity.UserMessageEntity
 import jed.choi.ui_core.UiState
 
 data class MySeatUiState(
-    val myName: String,
-    val myState: UserState,
+    val myName: String = "Unknown",
+    val myState: UserState = UserState.LOGGED_OUT,
     val mySeat: Seat? = null, // if before user reserve, this is null
-    val mySession: UserSession,
-    val userMessage: UserMessageEntity? = null,
+    val mySession: UserSession? = null, // if before user login, this is null
 ) : UiState
